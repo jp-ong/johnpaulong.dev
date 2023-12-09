541eb226fef0355c0adcda4285367d5d4b6eab69
John Paul Ong <jpong5202@gmail.com>
2023-12-09 20:10:59 +0800

    Add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "@/scss/globals.scss";
 
+import Script from "next/script";
+
 interface Props {
   children: React.ReactNode;
 }
@@ -7,6 +9,18 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.GA_ID}');
+        `}
+      </Script>
       <body>{children}</body>
     </html>
   );
