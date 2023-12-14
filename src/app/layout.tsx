169f5b76af6d7d35a47df455f9fc7faedde33151
John Paul Ong <jpong5202@gmail.com>
2023-12-14 23:24:27 +0800

    Add meta tags and robots.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import { Metadata } from "next";
+import Head from "next/head";
+
 import "@/scss/globals.scss";
 
 import { Sidebar } from "@/components/shared/sidebar";
@@ -6,6 +9,27 @@ import { GoogleAnalytics } from "@/components/shared/google-analytics";
 interface Props {
   children: React.ReactNode;
 }
+
+const title = "John Paul Ong | Web Developer";
+const description =
+  "Web development expert in Metro Manila. Translates designs into efficient systems. Delivers elegant solutions for clients and codebase for developers.";
+const baseUrl = "https://johnpaulong.dev";
+
+export const metadata: Metadata = {
+  title,
+  description,
+  metadataBase: new URL(baseUrl),
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title,
+    description,
+    siteName: title,
+    type: "website",
+    url: "/",
+  },
+};
 
 export default function RootLayout({ children }: Props) {
   return (
